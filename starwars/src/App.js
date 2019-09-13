fd94a79c129218d75c6345aa699cfd0ce2c4969e
Shaun Orpen <shaunorpen@users.noreply.github.com>
2019-09-13 10:23:23 +0100

    Add spinner

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -56,7 +56,48 @@ const App = () => {
         background: rgba(255,255,255,0.7);
       }
     }
-  `;
+
+    .loader,
+    .loader:after {
+      border-radius: 50%;
+      width: 10em;
+      height: 10em;
+    }
+    .loader {
+      margin: 15% auto;
+      font-size: 10px;
+      position: relative;
+      text-indent: -9999em;
+      border-top: 1.1em solid rgba(255, 255, 255, 0.2);
+      border-right: 1.1em solid rgba(255, 255, 255, 0.2);
+      border-bottom: 1.1em solid rgba(255, 255, 255, 0.2);
+      border-left: 1.1em solid #ffffff;
+      -webkit-transform: translateZ(0);
+      -ms-transform: translateZ(0);
+      transform: translateZ(0);
+      -webkit-animation: load8 1.1s infinite linear;
+      animation: load8 1.1s infinite linear;
+    }
+    @-webkit-keyframes load8 {
+      0% {
+        -webkit-transform: rotate(0deg);
+        transform: rotate(0deg);
+      }
+      100% {
+        -webkit-transform: rotate(360deg);
+        transform: rotate(360deg);
+      }
+    }
+    @keyframes load8 {
+      0% {
+        -webkit-transform: rotate(0deg);
+        transform: rotate(0deg);
+      }
+      100% {
+        -webkit-transform: rotate(360deg);
+        transform: rotate(360deg);
+      }
+    }`;
   
   if (apiResponse) {
     return (
@@ -80,7 +121,7 @@ const App = () => {
     return (
       <StyledApp>
         <h1>React Wars</h1>
-        <p>Loading...</p>
+        <div class="loader">Loading...</div>
       </StyledApp>
     );
   }
